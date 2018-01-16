c94816c552ee9fe70900e961aba90e8c0fe2924f
DESKTOP-SQ3LAVL\User <deb.jagadev@live.com>
2018-01-17 08:10:24 +1100

    Initial commit

---
+++ src/test/java/com/binisoft/features/HomePage.feature
@@ -0,0 +1,8 @@
+Feature: Home page
+  As an user when I navigate to index page
+  I will be able see the home page
+
+  @Regression
+  Scenario: Successfull load of the home page
+    Given as a user I  navigate to home page url
+    Then I will be able to see the home page
---
+++ src/test/java/com/binisoft/stepdefs/HomePageSteps.java
@@ -0,0 +1,21 @@
+package com.binisoft.stepdefs;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class HomePageSteps {
+
+    @Given("^as a user I  navigate to home page url$")
+    public void asAUserINavigateToHomePageUrl() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+    @Then("^I will be able to see the home page$")
+    public void iWillBeAbleToSeeTheHomePage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
